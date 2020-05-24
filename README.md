9f2038b146a47f6d90284474f66acbcb2c88f463
ambigo7 <dendirizkapoetra@gmail.com>
2020-05-24 23:46:23 +0700

    Versi 1.0.0

--- README.md
+++
@@ -1,2 +0,0 @@
-# Kill-Proccess-Program
-Kill Proccess atau End Task paksa program dengan python menggunakan timer di windows
---
+++ kill_timer.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+num = 0
+
+def waktu(tm):
+	time.sleep(tm)
+
+def main():
+	print("\n------Close Application With Timer In Python-----")
+	app = input("Application Name : ")
+	print("")
+	print("Program Execution Options: ")
+	print("1. Now")
+	print("2. Set Timer")
+	switch = input("Input your choice : ")
+	print("--------------------------------------------------")
+	if switch == "1":
+		global num
+		num = 0
+		#waktu(num)
+		print("Start the application end task...")
+	elif switch == "2":
+		print("----------------Options----------------")
+		print("A. 15 Minute")
+		print("B. 30 Minute")
+		print("C.  1 Hour")
+		print("D. Set Manually")
+		switch_time = input("Input your choice : ")
+		print("---------------------------------------")
+		if switch_time == "A" or switch_time == "a":
+			num = 900
+		elif switch_time == "B" or switch_time == "b":
+			num = 1800
+		elif switch_time == "C" or switch_time == "c":
+			num = 3600
+		elif switch_time == "D" or switch_time == "d":
+			num = int(input("Input Time[second] : "))
+	print("Start the application end task...")
+	print("End Task "+app+" in {} second".format(num))
+	waktu(num)
+	os.system("taskkill /f /im "+app+".exe")
+
+main()
+print("© 2020@dendirzkptr")
